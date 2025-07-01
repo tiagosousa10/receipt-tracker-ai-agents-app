f369fd4c9659e180a933fb5571197d53f30abc1e
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-01 14:29:58 +0100

    get temporary access token from schematic

--- actions/getTemporaryAccessToken.ts
+++ actions/getTemporaryAccessToken.ts
@@ -9,3 +9,25 @@ const apiKey = process.env.SCHEMATIC_API_KEY;
 const client = new SchematicClient({ apiKey });
 
 //get temporary access token
+export async function getTemporaryAccessToken() {
+  console.log("Getting temporary access token");
+  const user = await currentUser();
+
+  if (!user) {
+    console.log("No user found");
+    return null;
+  }
+
+  console.log(`Issuing temporary access token for user ${user.id}`);
+  const resp = await client.accesstokens.issueTemporaryAccessToken({
+    resourceType: "company",
+    lookup: { id: user.id },
+  });
+
+  console.log(
+    "Token response received:",
+    resp.data ? "Token received" : "Token not received",
+  );
+
+  return resp.data?.token;
+}
