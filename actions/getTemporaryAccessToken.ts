f45aedc1d6ef62b7bb5933e1b65c223eb2dd2f32
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-01 14:25:58 +0100

    install schematic

---
+++ actions/getTemporaryAccessToken.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+// Initialize Schematic SDK
+import { SchematicClient } from "@schematichq/schematic-typescript-node";
+
+const apiKey = process.env.SCHEMATIC_API_KEY;
+const client = new SchematicClient({ apiKey });
+
+//get temporary access token
