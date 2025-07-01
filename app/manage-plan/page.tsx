b49f5796713a0633477fc0aec240e2179680c83e
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-01 15:11:00 +0100

    configure ingest client and routes

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="container xl:max-w-5xl mx-auto p-4 md:p-0">
+      <h1 className="text-2xl font-bold mb-4 my-8">Manage Your Plan</h1>
+      <p className="text-gray-600 mb-8">
+        Manage your subscription and billing details here.
+      </p>
       <SchematicComponent
         componentId={
           process.env.NEXT_PUBLIC_SCHEMATIC_CUSTOMER_PORTAL_COMPONENT_ID
