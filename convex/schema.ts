0e905875aa7afec523b8542796fb34ab390ed16f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-03 18:25:47 +0100

    update schema for receipts

--- convex/schema.ts
+++ convex/schema.ts
@@ -15,5 +15,22 @@ export default defineSchema({
     size: v.number(),
     mimeType: v.string(),
     status: v.string(), // "pending", "success", "failed
+
+    //fields for extracted data
+    merchantName: v.optional(v.string()),
+    merchantAddress: v.optional(v.string()),
+    merchantContact: v.optional(v.string()),
+    transactionDate: v.optional(v.string()),
+    transactionAmount: v.optional(v.string()),
+    currency: v.optional(v.string()),
+    receiptSummary: v.optional(v.string()),
+    items: v.array(
+      v.object({
+        name: v.string(),
+        quantity: v.number(),
+        unitPrice: v.number(),
+        totalPrice: v.number(),
+      }),
+    ),
   }),
 });
