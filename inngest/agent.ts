52b77d52af111590236bb03d6e74ad8f7c17a12f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-07 15:12:40 +0100

    create the agent network and server

--- inngest/agent.ts
+++ inngest/agent.ts
@@ -3,5 +3,45 @@ import {
   createNetwork,
   getDefaultRoutingAgent,
 } from "@inngest/agent-kit";
+import { createServer } from "@inngest/agent-kit/server";
+import { inngest } from "./client";
+import Events from "./constants";
 
-const agentNetwork = createNetwork({});
+const agentNetwork = createNetwork({
+  name: "Agent Team",
+  agents: [databaseAgent, receitScanningAgent],
+  defaultModel: anthropic({
+    model: "claude-3-5-sonnet-latest",
+    defaultParameters: {
+      max_tokens: 1000,
+    },
+  }),
+  defaultRouter: ({ network }) => {
+    const savedToDatabase = network.state.kv.get("saved-to-database");
+
+    if (savedToDatabase !== undefined) {
+      // terminate the agent process if the data has been saved to the database.
+      return undefined;
+    }
+    return getDefaultRoutingAgent();
+  },
+});
+
+export const server = createServer({
+  agents: [databaseAgent, receitScanningAgent],
+  network: [agentNetwork],
+});
+
+export const extractAndSavePDF = inngest.createFunction(
+  {
+    id: "Extract PDF and Save in Database",
+  },
+  { event: Events.EXTRACT_DATA_FROM_PDF_AND_SAVE_TO_DATABASE },
+  async ({ event }) => {
+    const result = await agentNetwork.run(
+      `Extract the key data from this pdf: ${event.data.url}. Once the data is extracted, save it to the database using the receiptId : ${event.data.receiptId}. Once the receipt is successfully saved to the database you can terminate the agent process. Start with the Supervisor agent.`,
+    );
+
+    return result.state.kv.get("receipt");
+  },
+);
---
+++ inngest/constants.ts
@@ -0,0 +1,5 @@
+enum Events {
+  EXTRACT_DATA_FROM_PDF_AND_SAVE_TO_DATABASE = "extract_data_from_pdf_and_save_to_database",
+}
+
+export default Events;
