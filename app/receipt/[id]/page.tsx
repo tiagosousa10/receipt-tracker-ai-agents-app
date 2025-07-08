333190d62394b96f3257d88a49737320e46788a4
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-08 16:58:46 +0100

    fetching receipt details and download url

--- app/receipt/[id]/page.tsx
+++ app/receipt/[id]/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Receipt = () => {
   const params = useParams<{ id: string }>();
+  const [receiptId, setReceiptId] = useState<Id<"receipts"> | null>(null);
+  const router = useRouter();
+
+  //fetch the receipt details
+  const receipt = useQuery(
+    api.receipts.getReceiptById,
+    receiptId ? { id: receiptId } : "skip",
+  );
+
+  //get the download URL (for the view button)
+  const fileId = receipt?.fileId;
+  const downloadUrl = useQuery(
+    api.receipts.getReceiptDownloadUrl,
+    fileId ? { fieldId: fileId } : "skip",
+  );
+
+  useEffect(() => {
+    try {
+      const id = params.id as Id<"receipts">;
+      setReceiptId(id);
+    } catch (error) {
+      console.error("Invalid receipt id: ", error);
+      router.push("/");
+    }
+  }, [params.id, router]);
 
   return <div>receitp {params.id}</div>;
 };
