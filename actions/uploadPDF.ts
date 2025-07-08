7fee40229a398588ec9b3edb6111dd4c619471c4
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-08 16:51:08 +0100

    create the trigger for inngest

--- actions/uploadPDF.ts
+++ actions/uploadPDF.ts
@@ -4,6 +4,8 @@ import { api } from "@/convex/_generated/api";
 import convex from "@/lib/convexClient";
 import { currentUser } from "@clerk/nextjs/server";
 import { getFileDownloadUrl } from "./getFileDownloadUrl";
+import { inngest } from "@/inngest/client";
+import Events from "@/inngest/constants";
 
 export async function uploadPDF(formData: FormData) {
   const user = await currentUser();
@@ -66,6 +68,13 @@ export async function uploadPDF(formData: FormData) {
     const fileUrl = await getFileDownloadUrl(storageId);
 
     //TOD: trigger innges agent flow...
+    await inngest.send({
+      name: Events.EXTRACT_DATA_FROM_PDF_AND_SAVE_TO_DATABASE,
+      data: {
+        url: fileUrl.downloadUrl,
+        receiptId,
+      },
+    });
 
     return {
       success: true,
--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,7 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
+import { extractAndSavePDF } from "@/inngest/agent";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [extractAndSavePDF],
 });
