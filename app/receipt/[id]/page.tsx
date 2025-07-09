0d94cff7f4567716aa59217f63b9167310e4b853
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-09 18:56:26 +0100

    create a grid to show receipt details

--- app/receipt/[id]/page.tsx
+++ app/receipt/[id]/page.tsx
@@ -101,6 +101,63 @@ const Receipt = () => {
             Back to Receipts
           </Link>
         </nav>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden mb-6">
+          <div className="p-6">
+            <div className="flex items-center justify-baseline mb-6">
+              <h1 className="text-2xl font-bold text-gray-900 truncate">
+                {receipt.fileDisplayName || receipt.fileName}
+              </h1>
+              <div className="flex items-center ">
+                {receipt.status === "pending" ? (
+                  <div className="mr-2">
+                    <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-yellow-800"></div>
+                  </div>
+                ) : null}
+                <span
+                  className={`px-3 py-1 rounded-full text-sm ${receipt.status === "pending" ? "bg-yellow-100 text-yellow-800" : receipt.status === "processed" ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"}`}
+                >
+                  {receipt.status.charAt(0).toUpperCase() +
+                    receipt.status.slice(1)}
+                </span>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {/* information */}
+              <div className="space-y-4">
+                <div>
+                  <h3 className="text-sm font-medium text-gray-500">
+                    File Information
+                  </h3>
+                  <div className="mt-2 bg-gray-50 p-4 rounded-lg">
+                    <div className="grid grid-cols-2 gap-4 text-sm">
+                      <div>
+                        <p className="text-gray-500">Uploaded</p>
+                        <p className="font-medium">{uploadDate}</p>
+                      </div>
+                      <div>
+                        <p className="text-gray-500">Size</p>
+                        <p className="font-medium">
+                          {formatFileSize(receipt.size)}
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-gray-500">Type</p>
+                        <p className="font-medium">{receipt.mimeType}</p>
+                      </div>
+                      <div>
+                        <p className="text-gray-500">ID</p>
+                        <p className="font-medium truncate" title={receipt._id}>
+                          {receipt._id.slice(0, 10)}...
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
